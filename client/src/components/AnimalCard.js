947a07ef57636c97207be6b1c500290e3bfdca5e
Graeme Brown <graemebrown1874@gmail.com>
2021-12-13 21:58:25 +0000

    fixed issue with animal cards

--- client/src/components/AnimalCard.js
+++ client/src/components/AnimalCard.js
@@ -1,17 +1,9 @@
-//receives single animal object as json, it can show animal.name etc
-
-
 const AnimalCard = ({animal}) =>{
 
     return (
-    <> 
-    <p>Name: {animal.name}</p>
-    <p>Type: {animal.type}</p>
-
-
-     </>
-
-
+        <li className="animal-card">
+            {animal.name}
+        </li>
     )
 }
 export default AnimalCard;
