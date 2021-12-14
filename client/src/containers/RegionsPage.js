208f06d708774df3606d2e055e26e6048dc410b2
CptBroon <91729107+CptBroon@users.noreply.github.com>
2021-12-14 22:40:33 +0000

    Revert "Feature/diagonal styling"

--- client/src/containers/RegionsPage.js
+++ client/src/containers/RegionsPage.js
@@ -1,15 +1,37 @@
 import { useState } from "react";
 import AnimalCard from "../components/AnimalCard";
-import RegionCard from "../components/RegionCard";
 
 const RegionsPage = ({allAnimals, allRegions}) => {
     const [regionalAnimals, setRegionalAnimals] = useState([]);
-    const [filterRegion, setFilterRegion] = useState("");
     const [listFiltered, setListFiltered] = useState(false);
     
+    const handleFilterClick = (e) => {
+        const filteredList = []
+        allAnimals.map(animal => {
+            if (animal.region === e.target.value) {
+                filteredList.push(animal)
+            }
+        })
+        setRegionalAnimals(filteredList)
+        if (!listFiltered) {
+            setListFiltered(true)
+        }
+    }
+
+    const createRegionLink = (region) => {
+        console.log(region)
+        return (
+            <li className="region-title">
+                <button onClick={handleFilterClick} value={region}>
+                    {region[0].toUpperCase() + region.slice(1)}
+                </button>
+            </li>
+        )
+    }
+
     const generateRegionList = (regionList) => {
         if (allRegions.length) {
-            return regionList.map(region => <RegionCard region={region} listFiltered={listFiltered} setListFiltered={setListFiltered} allAnimals={allAnimals} setFilterRegion={setFilterRegion} setRegionalAnimals={setRegionalAnimals}/>)
+            return regionList.map(region => createRegionLink(region))
         }
     }
 
@@ -27,7 +49,7 @@ const RegionsPage = ({allAnimals, allRegions}) => {
             </ul>
             {listFiltered && <div id="filtered-list">
                 <h3> List of all animals by selected region </h3>
-                <ul id="filtered-animal-list">
+                <ul id="filtered-animal-list" className="flex-row">
                     {generateRegionalAnimalList(regionalAnimals)}
                 </ul>
             </div>}
