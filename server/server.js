24fa332bf6fbc87a0c01b5d345eaa0f8c6d2c024
Almas <m_a_b19@hotmail.com>
2021-12-14 11:57:46 +0000

    server changed back to 5000 from 5001

--- server/server.js
+++ server/server.js
@@ -17,6 +17,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
   })
   .catch(console.err);
 
-app.listen(5001, function() {
+app.listen(5000, function() {
     console.log(`Educational server running on port ${this.address().port}`);
   });
