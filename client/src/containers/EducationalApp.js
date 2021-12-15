fffa1aa229cee75040e78064a420b98bf86d361e
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-12-15 10:35:42 +0000

    login added

--- client/src/containers/EducationalApp.js
+++ client/src/containers/EducationalApp.js
@@ -3,6 +3,7 @@ import PageFooter from "../components/PageFooter";
 import HomePage from "./HomePage";
 import RegionsPage from "./RegionsPage";
 import ConservationPage from "./ConservationPage";
+import LoginPage from "./LoginPage";
 import PopupAd from "../components/PopupAd";
 import { useState, useEffect } from 'react';
 import React from "react";
@@ -18,6 +19,27 @@ const EducationalApp = () => {
     const [allAnimals, setAllAnimals] = useState([])
     const [allRegions, setAllRegions] = useState(["tundra", "desert", "ocean", "plains", "rainforest"])
     const [showPopup, setShowPopup] = useState(false);
+    const [user, setUser] = useState({name:""})
+    const [error, setError] = useState("")
+
+    const adminUser = {
+        name: "Almas",
+        password: "admin123"
+    }
+
+    const Login = (details) => {
+
+        if (details.name == adminUser.name && details.password == adminUser.password) {
+            setUser({name:details.name})
+        } else {
+            setError("Details do not match")
+        }
+
+    }
+
+    const Logout = () => {
+        setUser({name:""})
+    }
 
     const popUp = () => {
         if (!showPopup) {
@@ -42,7 +64,15 @@ const EducationalApp = () => {
             <Router>
             <header className="flex-column">
                 <div id="top-bar">
-                    Login
+                {(user.name != "") ? (
+                        <div className ="welcome">
+                            <p>Welcome, <span>{user.name}</span>
+                            <button onClick={Logout} id="logout-bttn">Log Out</button>
+                            </p> 
+                        </div>
+                    ) : (
+                        <Link to="/login"><i className="fas fa-sign-in-alt"></i> Log In</Link>
+                    )}
                 </div>
                 <div id="main-header" className="flex-row">
                     <div id="page-logo" className="flex-row">
@@ -68,6 +98,7 @@ const EducationalApp = () => {
                 <Route path="/home" element={<HomePage allAnimals={allAnimals}/>}/>
                 <Route path="/regions" element={<RegionsPage allRegions={allRegions} allAnimals={allAnimals}/>}/>
                 <Route path="/conservation" element={<ConservationPage/>}/>
+                <Route path="/login" element={<LoginPage Login={Login} error={error}/>}/>
             </Routes>
           <PageFooter />
           </Router>
